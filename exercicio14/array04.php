e3479f08f45bf04882c80545ece45fa92370cd47
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-30 12:08:22 -0300

    add exercise 14 - arrays

---
+++ exercicio14/array04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="../_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+  <div>
+    <pre>
+    <?php    
+      $m = array(array(0,3),
+                 array(5,4),
+                 array(6,3));  
+      print_r($m)
+    ?>
+    </pre>
+  </div>
+</body>
+
+</html>
---
+++ exercicio14/array05.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="../_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+  <div>
+    <pre>
+    <?php    
+      $m = array(array(0,3),
+                 array(5,4),
+                 array(6,3));  
+      print_r($m);
+
+      $m2 = array("A","B","C");  
+// print_r($m)
+var_dump($m2);
+    ?>
+    </pre>
+  </div>
+</body>
+
+</html>
