1d8d10c3740dc29c736fbaf18324984f54559350
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-31 18:42:05 -0300

    add aula04 - POO

--- POO/aula04/index.php
+++ POO/aula04/index.php
@@ -11,12 +11,9 @@
   <div>
     <pre>
     <?php
-      
       require_once 'Caneta.php';
       $c1 = new Caneta("BIC", "Vermelha", 0.5);
-      print_r($c1);
-    
-     
+      print_r($c1);  
     ?>
     </pre>
   </div>
